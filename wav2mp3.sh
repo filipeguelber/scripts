b9b8c1bc8a8a582bc2089ff42970bdb6247c7f02
Filipe Guelber Melo <filipe@filipe-laptop.(none)>
2014-03-25 14:47:10 -0300

    apenas um teste

--- wav2mp3.sh
+++ wav2mp3.sh
@@ -4,5 +4,6 @@
 # e-mail: douglasfs.ti@gmail.com
 # description: percorre o diretório atual e subdiretórios procurando arquivos wav
 #              e converte-os para mp3. Requer lame instalado
+#filipe
 
 find ./ -name "*.wav" -exec lame -V 0 -q 0 {} \;
